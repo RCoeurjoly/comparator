6408e0f2cb1d92b45435027d2d4b731269ad6f6b
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2023-01-03 20:01:42 +0100

    Se mejora el script para no usar cat

--- bme/__init__.py
+++ bme/__init__.py
@@ -1,20 +1,12 @@
-
 import os
 import sys
 import subprocess
 
+
 def main():
     args = sys.argv[1:]
-    first = open(args[0], "r")
-    second = open(args[1], "r")
+    subprocess.call(['graphtage -k --from-json --to-json <(wildq --ini "." ' + args[0] + ') <(wildq --ini "." ' + args[1] + ')'], shell=True)
 
-    #read whole files to a string
-    data1 = first.read()
-    data2 = second.read()
-    #close files
-    first.close()
-    second.close()
-    subprocess.call(['graphtage -k --from-json --to-json <(cat ' + args[0] + ' | wildq --ini ".") <(cat ' + args[1] + ' | wildq --ini ".")'], shell=True)
 
 if __name__ == '__main__':
-    print("Hello Poetry2nix!!")
+    main()

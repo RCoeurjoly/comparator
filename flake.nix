eb31daee52921a16e7f953528fa8f41453702ef7
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2023-01-03 17:16:27 +0100

    WIP

--- flake.nix
+++ flake.nix
@@ -35,7 +35,10 @@
         );
 
       };
-
+      my-name = "my-script";
+      my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./simple-script.sh)).overrideAttrs(old: {
+        buildCommand = "${old.buildCommand}\n patchShebangs $out";
+      });
       packageName = "BME";
     in {
 
@@ -61,11 +64,19 @@
 
         })
       ];
+      packages.x86_64-linux.my-script = pkgs.symlinkJoin {
+        name = my-name;
+        paths = [ my-script self.packages.x86_64-linux.default ];
+        buildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+                  wrapProgram $out/bin/${my-name} --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.coreutils self.packages.x86_64-linux.default ]}
+          '';
+      };
       packages.x86_64-linux.default = pkgs.myapp;
       devShells.x86_64-linux.default = pkgs.myAppEnv.env.overrideAttrs (oldAttrs: {
         buildInputs = with pkgs; [ poetry pkgs.geckodriver ];
       });
       apps.x86_64-linux.default = { type = "app"; program = "${self.packages.x86_64-linux.default}/bin/bme"; };
-      packages.x86_64-linux.bme_rpm = bundlers.bundlers.x86_64-linux.toRPM self.apps.x86_64-linux.default;
+      packages.x86_64-linux.bme_rpm = bundlers.bundlers.x86_64-linux.toRPM self.packages.x86_64-linux.default;
     };
 }
---
+++ simple-script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+which graphtage
+graphtage --version
+which wildq
+wildq --version
+which cat
+cat --version

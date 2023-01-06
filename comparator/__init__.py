90be7ba089fc9942b9f6cdf64d5d575bc2e2e1c7
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2023-01-06 18:07:12 +0100

    Se da soporte a comparar xml pequeños

--- comparator/__init__.py
+++ comparator/__init__.py
@@ -1,13 +1,36 @@
 import os
 import sys
 import subprocess
+import pathlib
 
+def get_wildq_command(extension):
+    return 'wildq --' + extension + ' \'walk(if type == "array" then sort else . end)\' '
+
+def graphtage_extra_args(extension1, extension2):
+    if (extension1 == 'xml' or extension2 == 'xml'):
+        return '--join-dict-items'
+    return ''
+
+def get_extensions(filename1, filename2):
+    if (filename1.endswith(".cfg") and filename2.endswith(".cfg")):
+        extension1 = 'ini'
+        extension2 = 'ini'
+    else:
+        extension1 = pathlib.Path(filename1).suffix[1:]
+        extension2 = pathlib.Path(filename2).suffix[1:]
+    return extension1, extension2
 
 def main():
     args = sys.argv[1:]
-    if (args[0].endswith() == ".cfg" and args[1].endswith() == ".cfg"):
-        return subprocess.call(['graphtage -k --from-json --to-json <(wildq --ini "." ' + args[0] + ') <(wildq --ini "." ' + args[1] + ')'], shell=True)
-    return subprocess.call(['graphtage -k ' + args[0] + ' ' + args[1]], shell=True)
+    filename1 = args[0]
+    filename2 = args[1]
+    rest_of_args = ''.join(sys.argv[3:])
+    extension1, extension2 = get_extensions(filename1, filename2)
+    return subprocess.call(['graphtage -k '
+                            + graphtage_extra_args(extension1, extension2)
+                            + ' --from-json --to-json '
+                            + '<(' + get_wildq_command(extension1) + filename1 + ') '
+                            + '<(' + get_wildq_command(extension2) + filename2 + ')'], shell=True)
 
 
 if __name__ == '__main__':
